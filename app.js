ea230e0fe63e72803f01db7079750db29cfff399
Leslie <leslie.ino@gmail.com>
2018-05-27 17:32:20 -0400

    1 avance

---
+++ app.js
@@ -0,0 +1,8 @@
+
+let age = prompt('Ingrese su edad');
+
+if ( age >= 1 && age <= 3) {
+    document.write("<div>" + age + " años" +  "= Toddler: Su hijo está avanzando desde la infancia hacia y hacia los años preescolares. Durante este tiempo, sucrecimiento físico y desarrollo motorse ralentizarán, pero puede esperarver cambios tremendos intelectuales, sociales y emocionales   </div>");
+    
+    alert('Perteneces a Toddler');
+}
